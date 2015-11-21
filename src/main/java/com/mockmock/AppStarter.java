e4f5ba4e8f0ab58fa1e5666ec8f3e78795314584
Koen Kuipers <koen@tweakers.net>
2015-11-21 15:08:09 +0100

    Removed test code

--- src/main/java/com/mockmock/AppStarter.java
+++ src/main/java/com/mockmock/AppStarter.java
@@ -3,15 +3,7 @@ package com.mockmock;
 import com.mockmock.console.Parser;
 import com.mockmock.server.Server;
 import org.springframework.beans.factory.BeanFactory;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-import java.util.Date;
-import java.util.Properties;
 
 public class AppStarter
 {
@@ -19,8 +11,7 @@ public class AppStarter
 
     public static void main(String[] args)
     {
-        ApplicationContext context = new ClassPathXmlApplicationContext("META-INF/beans.xml");
-        BeanFactory factory = context;
+        BeanFactory factory = new ClassPathXmlApplicationContext("META-INF/beans.xml");
 
         Settings settings = (Settings) factory.getBean("settings");
 
@@ -32,42 +23,8 @@ public class AppStarter
         smtpServer.setPort(settings.getSmtpPort());
         smtpServer.start();
 
-//		fillQueueWithTestMails(settings.getSmtpPort(), 20);
-
         Server httpServer = (Server) factory.getBean("httpServer");
         httpServer.setPort(settings.getHttpPort());
         httpServer.start();
     }
-
-	/**
-	 * Can be called to fill the queue with some test mails to make life easier
-	 * @param amount int
-	 */
-	protected static void fillQueueWithTestMails(int port, int amount)
-	{
-		for (int i = 0; i < amount; i++)
-		{
-			Properties properties = new Properties();
-			properties.put("mail.smtp.host", "localhost");
-			properties.put("mail.smtp.port", port);
-			properties.put("mail.debug", "true");
-
-			Session session = Session.getInstance(properties);
-
-			try
-			{
-				MimeMessage message = new MimeMessage(session);
-				message.setFrom(new InternetAddress("from@example.com"));
-				InternetAddress[] address = { new InternetAddress("to@example.com") };
-				message.setRecipients(MimeMessage.RecipientType.TO, address);
-				message.setSubject("This is just an automatic test mail (" + i + ")");
-				message.setSentDate(new Date());
-				message.setText("With some test content");
-
-				Transport.send(message);
-			}
-			catch (MessagingException e) {}
-		}
-
-	}
 }
