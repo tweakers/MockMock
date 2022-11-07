bca37af8df5eb28d7380036cd78536caad3ff0d6
Dominique Comte <dominique.comte.dev@gmail.com>
2022-11-07 22:34:08 +0100

    Fixed typo in m option description

--- src/main/java/com/mockmock/console/Parser.java
+++ src/main/java/com/mockmock/console/Parser.java
@@ -20,7 +20,7 @@ public class Parser
         Options options = new Options();
         options.addOption("p", true, "The mail port number to use. Default is 25000.");
         options.addOption("h", true, "The http port number to use. Default is 8282.");
-        options.addOption("m", true, "The maximum size of the mail qeueue. Default is 1000.");
+        options.addOption("m", true, "The maximum size of the mail queue. Default is 1000.");
         options.addOption("c", true, "Comma separated list of channels. Default is #postman.");
         options.addOption("ff", true, "Filters out from email addresses (comma separated).");
         options.addOption("ft", true, "Filters out to email addresses (comma separated).");
